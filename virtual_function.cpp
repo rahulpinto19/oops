fb8f42f49440b23eb63ed5e1778b92079328276d
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2023-06-26 20:03:08 +0530

    Create virtual_function.cpp

---
+++ virtual_function.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+class parent
+{
+public:
+    virtual void  show()
+    {
+        cout << "this is the show of parent class"<<endl;
+    }
+    void print()
+    {
+        cout << "this is the print of the parent class"<<endl;
+    }
+};
+class child : public parent
+{
+public:
+    void show()
+    {
+        cout << "this is the show of the child class"<<endl;
+    }
+    virtual void print()
+    {
+        cout << "this is the print of the child class"<<endl;
+    }
+};
+int main()
+{
+    parent *bptr;
+    child d;
+    bptr = &d;
+
+    bptr->show();
+
+    bptr->print();
+}
